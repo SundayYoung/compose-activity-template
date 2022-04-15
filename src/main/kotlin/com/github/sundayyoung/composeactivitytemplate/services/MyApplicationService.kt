4268fb996cf3c9ae68aa7a5558b0f0c8c14b9cc5
GitHub Action <action@github.com>
2022-04-15 02:34:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sundayyoung/composeactivitytemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sundayyoung.composeactivitytemplate.services
+
+import com.github.sundayyoung.composeactivitytemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
